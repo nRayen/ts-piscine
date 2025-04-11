e78f10f67a0ea41746aa063a3dc30331e5a27fc8
nRayen <rayen.ndiaye@gmail.com>
2025-04-11 17:02:50 +0200

    S1 EP2

--- src/personne.ts
+++ src/personne.ts
@@ -1,15 +1,48 @@
+interface Adresse {
+    rue: string,
+    ville: string
+}
+
 interface Personne {
+    readonly id: number,
     nom: string,
-    age: number
+    age: number,
+    adresse?: Adresse
 };
 
 const afficherPersonne = (p: Personne) => {
     console.log("Bonjour " + p.nom + ". Tu as " + p.age + " ans.")
 }
 
+const afficherPersonneAvancee = (p: Personne) => {
+    if (p.adresse) {
+        console.log(`Bonjour, je m'appelle ${p.nom} (ID: ${p.id}), j'ai ${p.age} ans et j'habite au ${p.adresse.rue}, ${p.adresse.ville}.`)
+    } else {
+        console.log(`Bonjour, je m'appelle ${p.nom} (ID: ${p.id}) et j'ai ${p.age} ans.`)
+    }
+}
+
 const personne1 : Personne = {
+    id: 1,
     nom: "Alice",
     age: 25
 }
 
-afficherPersonne(personne1)
+const personne2 : Personne = {
+    id: 2,
+    nom: "Jean",
+    age: 34,
+    adresse: {
+        rue: "16 avenue du Maréchal",
+        ville: "Los Santos"
+    }
+}
+
+const personne3 : Personne = {
+    id: 3,
+    nom: "Marie",
+    age: 18
+}
+
+afficherPersonneAvancee(personne2)
+afficherPersonneAvancee(personne3)
