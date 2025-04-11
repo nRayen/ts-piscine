0cdea6c3f708935988e09251bfaf68388871cde4
nRayen <rayen.ndiaye@gmail.com>
2025-04-11 23:42:11 +0200

    S4 EP1

---
+++ dist/animaux.js
@@ -0,0 +1,13 @@
+"use strict";
+class Animal {
+    constructor(nom) {
+        this.seDeplacer = () => {
+            console.log(`L'animal ${this.nom} se déplace.`);
+        };
+        this.nom = nom;
+    }
+}
+const lion = new Animal("Mufasa");
+const lionceau = new Animal("Simba");
+lion.seDeplacer();
+lionceau.seDeplacer();
---
+++ src/animaux.ts
@@ -0,0 +1,21 @@
+interface iAnimal {
+    nom: string,
+    seDeplacer(): void
+}
+
+class Animal implements iAnimal {
+    nom: string
+
+    constructor (nom: string) {
+        this.nom = nom
+    }
+
+    seDeplacer = () : void => {
+        console.log(`L'animal ${this.nom} se déplace.`)
+    }
+}
+
+const lion = new Animal("Mufasa")
+const lionceau = new Animal("Simba")
+lion.seDeplacer()
+lionceau.seDeplacer()
