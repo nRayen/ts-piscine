fcd3b1f625500d9ad9be826387c6c9b9fc1d31ed
nRayen <rayen.ndiaye@gmail.com>
2025-04-12 14:34:57 +0200

    S5 EP1

---
+++ dist/S5/EP1/tuples.d.ts
@@ -0,0 +1,3 @@
+declare function creerScore(joueur: string, score: number): [string, number];
+declare function afficherScore([nom, score]: [string, number]): void;
+declare let scoreJ1: [string, number];
---
+++ dist/S5/EP1/tuples.js
@@ -0,0 +1,10 @@
+"use strict";
+function creerScore(joueur, score) {
+    return [joueur, score];
+}
+function afficherScore([nom, score]) {
+    // let [nom,nombre] = score
+    console.log(`Le joueur ${nom} a un score de ${score}`);
+}
+let scoreJ1 = creerScore("Rayen", 222);
+afficherScore(scoreJ1);
