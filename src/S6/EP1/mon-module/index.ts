e525529075dd9bce9d107f082af939618147eb5e
nRayen <rayen.ndiaye@gmail.com>
2025-04-14 22:27:45 +0200

    removing other season files

--- src/S6/EP1/mon-module/index.ts
+++
@@ -1,9 +0,0 @@
-function alerter(message: string) {
-    console.error("Alerte:", message);
-  }
-  
-  function logger(message: string) {
-    console.log("Log:", message);
-  }
-  
-  export { alerter, logger };
