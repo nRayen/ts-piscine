b50bdb9104e6e9d71251c886ac1d91f45cf58a42
nRayen <rayen.ndiaye@gmail.com>
2025-04-14 22:24:21 +0200

    removing other season files

--- dist/S6/EP1/mon-module/index.d.ts
+++
@@ -1,3 +0,0 @@
-declare function alerter(message: string): void;
-declare function logger(message: string): void;
-export { alerter, logger };
--- src/S6/EP1/mon-module/mon-module.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "mon-module" {
-    export function alerter(message: string) : void;
-    export function logger(message: string) : void
-}
